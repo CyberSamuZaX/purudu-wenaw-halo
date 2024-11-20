a8c70b09fddfe5389d8ccf56950cf1f91d24062b
Mr.Pasindu <140577992+CyberSamuZaX@users.noreply.github.com>
2024-11-20 16:34:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,8 +17,6 @@ const util = require('util')
 const { sms,downloadMediaMessage } = require('./lib/msg')
 const axios = require('axios')
 const { File } = require('megajs')
-const prefix = '.'
-
 const ownerNumber = ['94765718443']
 
 //===================SESSION-AUTH============================
@@ -45,6 +43,10 @@ const connectDB = require('./lib/mongodb')
 connectDB();
 
 //===========================================
+const {readEnv} = require('./lib/database')
+const config = await readEnv();
+const prefix = config.PREFIX
+        ///=====================================////
         
 console.log("Connecting wa bot 🧬...");
 const { state, saveCreds } = await useMultiFileAuthState(__dirname + '/auth_info_baileys/')
