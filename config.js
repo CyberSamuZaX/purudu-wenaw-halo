56be4f3e2db45b9d78931af97f2e7d6f759ce5a2
Mr.Pasindu <140577992+CyberSamuZaX@users.noreply.github.com>
2024-11-20 16:24:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
-ALIVE_IMG: process.env.ALIVE_IMG ││ "https://camo.githubusercontent.com/0e8e5114bac1af7227569baa78a746b6c52b7f6fcfc8752a14c909712dcef66d/68747470733a2f2f692e696d6775722e636f6d2f6c614342786e612e706e67.png"
-ALIVE_MSG: process.env.ALIVE_MSG ││ "I am Dark samuza"
+MONGODB: process.env.MONGODB ⎸⎸ "enter mongodb here",
+
 };
