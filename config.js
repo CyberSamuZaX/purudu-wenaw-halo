beda4e3d436820cb55aa902312698b718ac9e852
Mr.Pasindu <140577992+CyberSamuZaX@users.noreply.github.com>
2024-11-30 17:41:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,8 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "33QGkbxY#RA0TENww2V5DhSIVvEPxUwhkKi6RD8LHnzsUt7LuA9E",
 MONGODB: process.env.MONGODB || "mongodb://mongo:kSbfesNZJTcKFugNruFdhClchRYFMSnj@mongodb.railway.internal:27017",
-MODE: pprocess.env.MODE || "public"
+ALIVE_IMG: process.env.ALIVE_IMG || "https://avatars.githubusercontent.com/u/140577992?v=4.jpg"
+MODE: process.env.MODE || "public"
 
 };
 
